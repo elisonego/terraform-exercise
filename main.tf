a0245b170b2ce839236f250511fed50044626d4c
elisonego <113848756+elisonego@users.noreply.github.com>
2023-05-18 20:46:03 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,17 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 resource "aws_instance" "example" {
-    ami = "ami-0aa2b7722dc1b5612"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "terraform-instance"
-    }}
- resource "aws_s3_bucket" "my_bucket" {
-  bucket = "thisismybucketcreatedterraf"  # Update with your desired bucket name
-  acl    = "private"                # Access control for the bucket (optional)
+  ami           = "ami-0aa2b7722dc1b5612"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "terraform-instance"
+  }
 }
 
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "thisismybucketcreatedterraf"  # Update with your desired bucket name
+  acl    = "private"                      # Access control for the bucket (optional)
+}
