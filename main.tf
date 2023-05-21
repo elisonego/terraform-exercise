c52cd72662cf81ec2494b5329ad4739e4ca67eaa
elisonego <113848756+elisonego@users.noreply.github.com>
2023-05-21 19:07:54 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ resource "aws_s3_bucket" "my_bucket" {
 resource "aws_s3_bucket_public_access_block" "my_bucket_access_block" {
   bucket = aws_s3_bucket.my_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
