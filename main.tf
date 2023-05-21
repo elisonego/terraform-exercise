6ead1acb2a52faf5612656585677093176edf3a5
elisonego <113848756+elisonego@users.noreply.github.com>
2023-05-21 18:23:25 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,12 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "thisismybucketcreatedterraf"  # Update with your desired bucket name
   acl    = "private"                      # Access control for the bucket (optional)
 }
+
+resource "aws_s3_bucket_public_access_block" "my_bucket_access_block" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
